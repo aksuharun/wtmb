0019998d20fc8c5bb9409460675203dd3cd1fdb0
Harun Aksu <haksu@k1m43s04.42istanbul.com.tr>
2023-07-04 20:46:00 +0300

    Change readFileSync funtion to readFile (async) version

--- database.js
+++ database.js
@@ -1,7 +1,16 @@
 import fs from 'fs'
 
-const load = function(filename){
-	return JSON.parse(fs.readFile(filename,'utf8'))
+const load = function(filename,callback){
+	fs.readFile(filename,'utf8',(err, file) => {
+		console.log('error:'+err,"file:"+file)
+		if(err){
+			console.log("There is a read error")
+			callback(err)
+			return
+		}else{
+			callback(null,JSON.parse(file));
+		}
+	})
 }
 
 const save = function(filename,data){
--- index.js
+++ index.js
@@ -5,8 +5,16 @@ import {save, load} from './database.js'
 const harun = new User('aksuharun', 'Harun', 'Aksu', 19)
 const rumeysa = new User('rakpinar', 'Rumeysa Nur', 'Akpinar', 18)
 
+const callback = function(err,loadedFile){
+    if(err){
+        console.log('An error occured',err);
+        return
+    }else{
+        console.log('file loaded')
+        const nodejs = Group.create(loadedFile)
+        console.log(nodejs)
+    }
+}
 
-const loadedFile = load('groups.json')
-
-const nodejs = Group.create(loadedFile)
-
+load('groups.json',callback)
+console.log('hi');
