f8c58543f672ba554e1c25510db8f7c47285206e
aksuharun <harun16aksu@gmail.com>
2023-10-01 23:36:21 +0300

    Add book adding, viewing features.

--- HTTP-APIs/index.js
+++ HTTP-APIs/index.js
@@ -1,20 +1,53 @@
 import express from 'express'
 import VisitorService from './services/visitor-service.js'
+import BookService from './services/book-service.js'
 import Visitor from './models/visitor.js'
-import bodyParser from 'body-parser'
+import Book from './models/book.js'
 
 const port = 3000; 
 const hostname = '127.0.0.1';
 const app = express()
-
 const pages = ['Books', 'Contact', 'Login'] 
 
 app.set('view engine', 'pug')
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
 
 app.get('/', (req, res) => {
 	res.render('index', {pages:pages})
 })
+
+//Add book
+
+app.get('/book/add', (req, res) => {
+	res.render('add-book')
+})
+
+app.post('/book/add', async (req, res) => {
+	console.log(req.body)
+	const book = new Book(
+		req.body.name,
+		req.body.author,
+		req.body.publisher,
+		req.body.page,
+		req.body.img_url
+		)
+		await BookService.add(book)
+		res.redirect('/book/add')
+	})
+
+	//	View Books
+	
+	app.get('/book/all', async (req, res) => {
+		const allBooks = await BookService.findAll()
+		res.render('books', {books: allBooks})
+	})
+	
+	app.get('/book/:id', async (req, res) => {
+		const id = req.params.id
+		const book = await BookService.find(id)
+		res.render('book', {book: book})
+	})
 
 //	View Visitors
 app.get('/visitor/all', async (req, res) => {
@@ -26,24 +59,18 @@ app.get('/visitor/:id', async (req, res) => {
 	const id = req.params.id
 	const visitor = await VisitorService.find(id)
 
-	res.render('visitor',{visitor: visitor})
-})
-
-//Add book
-
-app.get('/book/add', async (req, res) => {
-  res.render('add-book')
+	res.render('visitor', {visitor: visitor})
 })
 
 
 //	Add visitor
 
-app.post('/visitor/add',  async (req, res) => {
+app.post('/visitor/add',	async (req, res) => {
 	console.log(req.body)
 	await VisitorService.add(req.body)
 })
 
-//  Delete visitor
+//	Delete visitor
 
 app.delete('/visitor/:id', async (req, res) => {
 	await VisitorService.del(req.params.id)
--- HTTP-APIs/models/book.js
+++ HTTP-APIs/models/book.js
@@ -1,13 +1,15 @@
 const Book = class{
-	constructor(name, author, page, img, tags = [], currentOwner = null, id) {
+	constructor(name, author, publisher, page, img, currentOwner, id) {
 		this.name = name
+		this.publisher = publisher
 		this.author = author
 		this.page = page
+		this.img = img
 		this.id = id
 	}
 
-	static create({name, author, page, img, tags, currentOwner, id}) {
-		return new Book(name, author, page, img, tags, currentOwner, id)
+	static create({name, author, publisher, page, img, currentOwner, id}) {
+		return new Book(name, author, publisher, page, img, currentOwner, id)
 	}
 }
 
